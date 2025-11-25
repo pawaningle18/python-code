014bf66caae443c4d1f54d37828562317db65f0f
pawaningle18 <inglepawan192@gmail.com>
2025-11-25 15:09:48 +0530

    Add files via upload

---
+++ dictionary accessing.py
@@ -0,0 +1,12 @@
+# Creating a dictionary
+my_dict = {
+    "name": "Alice",
+    "age": 25,
+    "city": "New York"
+}
+
+# Accessing elements
+print("Name:", my_dict["name"])
+print("Age:", my_dict["age"])
+print("City:", my_dict["city"])
+
