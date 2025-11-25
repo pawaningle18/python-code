2bf7d2ff8d9797da885fb7c41fc0133b63cff7c2
pawaningle18 <inglepawan192@gmail.com>
2025-11-25 15:25:59 +0530

    Add files via upload

---
+++ reverse string  slicing.py
@@ -0,0 +1,11 @@
+# Input string
+s = input("Enter a string: ")
+
+# Reverse using a loop
+reversed_s = ""
+for char in s:
+    reversed_s = char + reversed_s
+
+# Print reversed string
+print("Reversed string:", reversed_s)
+
