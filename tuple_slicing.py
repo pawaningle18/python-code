392b00db4783b0b7773ffa5566b0660328b83806
pawaningle18 <inglepawan192@gmail.com>
2025-11-25 14:47:04 +0530

    Add files via upload

---
+++ tuple_slicing.py
@@ -0,0 +1,13 @@
+# Create a tuple
+my_tuple = (10, 20, 30, 40, 50)
+
+# Slice the tuple
+slice1 = my_tuple[:3]         # First three elements
+slice2 = my_tuple[1:4]        # Middle elements
+slice3 = my_tuple[3:]         # Last two elements
+
+# Print slices
+print(slice1)
+print(slice2)
+print(slice3)
+
