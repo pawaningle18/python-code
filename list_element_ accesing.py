4fb6c4854f738b1e300037c3d6cc4511721974c2
pawaningle18 <inglepawan192@gmail.com>
2025-11-25 14:24:45 +0530

    Add files via upload

---
+++ list_element_ accesing.py
@@ -0,0 +1,15 @@
+# Create a list
+my_list = [10, 20, 30, 40, 50]
+
+# Access elements by index
+print("First item:", my_list[0])     
+print("Third item:", my_list[2])     
+print("Last item (negative index):", my_list[-1])  
+
+# Access a slice (range) of the list
+print("Items from index 1 to 3:", my_list[1:4])  
+
+print("All elements:")
+for item in my_list:
+    print(item)
+
