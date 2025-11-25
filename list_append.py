98ab3c8f00eea715944ca059e5eb8650721507e9
pawaningle18 <inglepawan192@gmail.com>
2025-11-25 14:34:41 +0530

    Add files via upload

---
+++ list_append.py
@@ -0,0 +1,11 @@
+# Create an empty list
+my_list = []
+
+# Append some elements
+my_list.append(10)
+my_list.append(20)
+my_list.append("hello")
+
+# Print the list
+print("List after appending:", my_list)
+
