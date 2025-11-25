0ccda3c375aab50ddb0ef52dd1640d0937974174
pawaningle18 <inglepawan192@gmail.com>
2025-11-25 15:00:13 +0530

    Update arithmetic_operation.py

--- arithmetic_operation.py
+++ arithmetic_operation.py
@@ -1,10 +1,9 @@
-# Numbers
-a = 10
-b = 5
+# Take input from user
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
 
 # Basic arithmetic
 print("Addition:", a + b)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
 print("Division:", a / b)
-
