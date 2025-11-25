8412c1eb7ed48665e9a1c01af18130ee19b24f8f
pawaningle18 <inglepawan192@gmail.com>
2025-11-25 15:18:34 +0530

    Add files via upload

---
+++ STRING AND LENGTH.py
@@ -0,0 +1,9 @@
+# Input a string from user
+user_input = input("Enter a string: ")
+
+# Find the length of the string
+length = len(user_input)
+
+# Print the length
+print("Length of the string is:", length)
+
