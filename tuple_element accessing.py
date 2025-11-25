5d50ca0668e83541336360e84f24956dbf75d5ff
pawaningle18 <inglepawan192@gmail.com>
2025-11-25 14:43:50 +0530

    Add files via upload

---
+++ tuple_element accessing.py
@@ -0,0 +1,9 @@
+# Creating a tuple
+my_tuple = (10, 20, 30, 40, 50)
+
+# Accessing elements by index
+print(my_tuple[0])   # First element -> 10
+print(my_tuple[2])   # Third element -> 30
+print(my_tuple[-1])  # Last element -> 50
+print(my_tuple[-2])  # Second last element -> 40
+
