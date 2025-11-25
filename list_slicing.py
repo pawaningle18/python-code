c3f4daae2e7cdb867d4abd2ed5a9f9a161b22e76
pawaningle18 <inglepawan192@gmail.com>
2025-11-25 14:28:14 +0530

    Add files via upload

---
+++ list_slicing.py
@@ -0,0 +1,14 @@
+# Example list  
+my_list = [10, 20, 30, 40, 50, 60, 70]
+
+# Basic slices
+print("First three elements:", my_list[0:3])     
+print("From index 2 to end:", my_list[2:])        
+print("Up to index 4 (exclusive):", my_list[:4])  
+
+# Using a step
+print("Every second element:", my_list[::2])      
+
+# Reverse the list using slicing
+print("Reversed list:", my_list[::-1])            
+
