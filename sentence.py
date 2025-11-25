f1ca8f0798a1d92f890b2dd8b14f1b27671a7faf
pawaningle18 <inglepawan192@gmail.com>
2025-11-25 15:22:29 +0530

    Add files via upload

---
+++ sentence.py
@@ -0,0 +1,12 @@
+# Input sentence from user
+sentence = input("Enter a sentence: ")
+
+# Split sentence into words
+words = sentence.split()
+
+# Count number of words
+num_words = len(words)
+
+# Print result
+print("Number of words in the sentence:", num_words)
+
