ea5f6eaf06183e65a366d21847c1accc523f1f44
pawaningle18 <inglepawan192@gmail.com>
2025-11-27 14:28:46 +0530

    Add files via upload

---
+++ 2 table in triangle pattern.py
@@ -0,0 +1,11 @@
+# Increasing triangle pattern with 2 multiplication table numbers
+
+rows = int(input("Enter number of rows: "))
+num = 2  # start from 2
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(num, end=" ")
+        num += 2
+    print()
+
