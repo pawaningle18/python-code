33119382f68b22e99db0e74e0576c1a5f9a17495
pawaningle18 <inglepawan192@gmail.com>
2025-11-27 14:45:34 +0530

    Update arithmetic_operation.py
    Removed user input prompts from the arithmetic operations.

--- arithmetic_operation.py
+++ arithmetic_operation.py
@@ -1,8 +1,7 @@
-# Take input from user
+
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 
-# Basic arithmetic
 print("Addition:", a + b)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
