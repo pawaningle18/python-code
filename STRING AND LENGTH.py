822021f89c656d296f1998bbd4b8bc647f913d69
pawaningle18 <inglepawan192@gmail.com>
2025-11-27 14:44:58 +0530

    Clean up STRING AND LENGTH.py
    Removed unnecessary comments and whitespace.

--- STRING AND LENGTH.py
+++ STRING AND LENGTH.py
@@ -1,9 +1,4 @@
-# Input a string from user
 user_input = input("Enter a string: ")
-
-# Find the length of the string
 length = len(user_input)
-
-# Print the length
 print("Length of the string is:", length)
 
