de6f3005d84e8b9011d66779fec254bd522684c3
pawaningle18 <inglepawan192@gmail.com>
2025-11-27 14:00:31 +0530

    Add files via upload

---
+++ string uppercase and lowercase.py
@@ -0,0 +1,10 @@
+# Program to convert a string to uppercase and lowercase
+
+text = input("Enter a string: ")
+
+uppercase_text = text.upper()
+lowercase_text = text.lower()
+
+print("Uppercase:", uppercase_text)
+print("Lowercase:", lowercase_text)
+
